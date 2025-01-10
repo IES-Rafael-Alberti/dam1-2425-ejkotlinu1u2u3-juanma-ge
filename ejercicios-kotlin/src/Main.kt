f606693e09880f9b9ed2c573983fb8de22c5cd74
juanma-ge <jgomest545@g.educaand.es>
2025-01-10 23:39:45 +0100

    He intentadi el menú pero creo que no se importar los paquetes correctamente, hago push y seguiré investigándolo.

--- ejercicios-kotlin/src/Main.kt
+++ ejercicios-kotlin/src/Main.kt
@@ -1,14 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+package
+import U1
+import U2
+import U3
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    while (true) {
+        println(
+            """
+            |========================
+            |       KOTLIN BÁSICO
+            |========================
+            |1. Ej-U1
+            |2. Ej-U2
+            |3. Ej-U3
+            |4. SALIR
+            |========================
+            |Elige una opción:
+        """.trimMargin()
+        )
+        when (readln().toIntOrNull()) {
+            1 -> submenuU1()
+            2 -> submenuU2()
+            3 -> submenuU3()
+            4 -> {
+                println("Saliendo del programa...")
+                break
+            }
+            else -> println("Opción inválida, intenta de nuevo.")
+        }
     }
 }
+
+fun submenuU1() {
+    while (true) {
+        println(
+            """
+            |========================
+            |       U1 - Básicos
+            |========================
+            |1. Ejercicio 4
+            |2. Ejercicio 6
+            |3. Ejercicio 12
+            |4. Ejercicio 15
+            |5. Ejercicio 18
+            |6. Ejercicio 20
+            |7. Ejercicio 21
+            |8. Ejercicio 22
+            |9. Ejercicio 24
+            |10. Ejercicio 25
+            |11. Ejercicio 26
+            |12. Ejercicio 27
+            |X. VOLVER
+            |========================
+            |Elige una opción:
+        """.trimMargin()
+        )
+        when (readln()) {
+            "1" ->
+            "2" ->
+            "3" ->
+            "4" ->
+            "5" ->
+            "6" ->
+            "7" ->
+            "8" ->
+            "9" ->
+            "10" ->
+            "11" ->
+            "12" ->
+            "X", "x" -> break
+            else -> println("Opción inválida, intenta de nuevo.")
+        }
+    }
+}
+
--- ejercicios-kotlin/src/U3/Main.kt
+++ ejercicios-kotlin/src/U3/Main.kt
@@ -1,2 +1,11 @@
 package U3
 
+class Persona(val nombre : String, val edad: Int){
+
+}
+
+fun main () {
+
+    val persona1 = Persona(nombre = "Diego", edad = 51)
+
+}
